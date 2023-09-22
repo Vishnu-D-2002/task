cd35c499fd778eb874396f15cbfbbd281bdfdbfa
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-09-22 12:37:25 +0530

    file add

---
+++ task-10/script.js
@@ -0,0 +1,22 @@
+
+let displayScreen = function(num, callback){
+    if (num === 0) {
+        callback();
+    }
+    else {
+        setTimeout(() => {
+            document.getElementById('display').innerText = num;
+            displayScreen((num - 1), callback);
+        },1000)
+    }
+}
+
+displayScreen(10,() => {
+    document.getElementById('display').innerHTML =
+     `<p><span id="happy">HAPPY</span></p>
+    <p><span id="independence">INDEPEDENCE</span> </p>
+    <p><span id="day">DAY</span></p>`
+    //  `HAPPY
+    //  INDEPENDENCE
+    // DAY`;
+}); 
