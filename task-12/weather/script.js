6876b19da535793c97e400e0b8a466e2ff7d3675
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-09-30 11:41:09 +0530

    file add

--- task-12/weather/script.js
+++ task-12/weather/script.js
@@ -1,29 +1,29 @@
-            let place = document.querySelector(".input");
-            let DisplayWeather = document.querySelector(".DisplayWeather");
-            let details = document.querySelector(".details");
+let place = document.querySelector(".input");
+let DisplayWeather = document.querySelector(".DisplayWeather");
+let details = document.querySelector(".details");
 
-            DisplayWeather.addEventListener('click', (event) => {
-                event.preventDefault();
-                //to prevent form default
+DisplayWeather.addEventListener('click', (event) => {
+    event.preventDefault();
+    //to prevent form default
 
-                const cityName = place.value;
-                const apiKey = "52cec576c37b154af74cc701da1bd04d";
-                //api key for fetching data
+    const cityName = place.value;
+    const apiKey = "52cec576c37b154af74cc701da1bd04d";
+    //api key for fetching data
 
-                fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
-                    .then((res) => res.json())
-                    // for convert to object
-                    
-                    .then((data) => {
-                        details.innerHTML = "";
-                        const temperature = data.main.temp;
-                        let weatherDescription = data.weather[0].main;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
+        .then((res) => res.json())
+        // for convert to object
+        
+        .then((data) => {
+            details.innerHTML = "";
+            const temperature = data.main.temp;
+            let weatherDescription = data.weather[0].main;
 
-                        let temp = document.createElement('p');
-                        temp.innerHTML = `<b>Temperature: ${temperature}℃</b><br><b>☁️${weatherDescription}</b>`;
-                        details.appendChild(temp);
-                    })
-                    .catch(error => {
-                        console.log('Enter correct city name', error);
-                    });
-            });
+            let temp = document.createElement('p');
+            temp.innerHTML = `<b>Temperature: ${temperature}℃</b><br><b>☁️${weatherDescription}</b>`;
+            details.appendChild(temp);
+        })
+        .catch(error => {
+            console.log('Enter correct city name', error);
+        });
+});
